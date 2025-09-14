6e771faa1088450e08d28d52e553f5a5e8aa94a0
GetBoros <getboros@gmail.com>
2025-09-14 11:25:40 +0300

    feat: Initial project setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+
+    // ждём пока пользователь нажмёт Enter
+    // std::cout << "Press enter to continued!" << std::endl;
+    // std::cin.get();
+
+    return 0;
+}
