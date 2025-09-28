336e662dac0ec27083279522c0af63e2ccf3602b
GetBoros <getboros@gmail.com>
2025-09-28 16:19:36 +0300

    refact: Add to example_task examples with threads, mtx, and condition variable

--- lib_examples/src/lib_examples.cpp
+++ lib_examples/src/lib_examples.cpp
@@ -8,24 +8,7 @@
 //------------------------------------------------------------------------------------------------------------
 LIB_EXAMPLES_API void FLib_Examples()
 {
-    AExample_Task example_task;
-       /*
-    std::jthread thread_worker(Thread_Func);
-
-    // 1.0. Wait untill mutex get free and try to change go(can do work) and after unlock
-    {
-        std::this_thread::sleep_for(std::chrono::seconds(1) );  // not important
-
-        std::lock_guard<std::mutex> lock(mtx);
-        go = true;
-    }
+    // AExample_Task example_task;
     
-    cv.notify_one();  // send signal
-
-    // 1.1. 
-    std::cout << "[Главный поток] Сигнал отправлен. Жду еще 6 секунд перед выходом." << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(6) );
-    std::cout << "[Главный поток] Завершаю работу. jthread автоматически остановит и присоединит рабочий поток." << std::endl;
-    */
 }
 //------------------------------------------------------------------------------------------------------------
